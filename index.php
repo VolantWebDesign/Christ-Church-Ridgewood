142925b00fcfcefe2cbdfa02c1e972c54b54a463
Tyler Stahl <tyler@volantweb.com>
2017-11-06 10:13:47 -0500

    Updated documents for 11/5 worship, and removed Rainbow Room event card

--- index.php
+++ index.php
@@ -120,26 +120,7 @@
 			<div class="row">
 				<div class="col-md-12" align="center">
 					<h2 style="color:#113056">Upcoming Events</h2>
-				</div>
-
-<!-- Rainbow Room 2017 -->
-				<div class="col-md-6">
-					<div class="card card-default">
-						<div class="card-body" align="center">	
-
-						<h3>Event Canceled</h3>
-
-							<img src="documents/ccfalldance2017vlarge.png" alt="The Rainbow Room, Dinner and Dancing" title="The Rainbow Room, Dinner and Dancing" class="img-fluid">
-							<br>
-
-						</div>
-						<div class="card-footer" align="center">
-							For more information, contact Al Pucciarelli at <a href="mailto:apucciarelli@mdmc-law.com">apucciarelli@mdmc-law.com</a>
-						</div>
-					</div>
-				</div>
-
-					<br>          
+				</div>         
 
 <!-- African Drum Ensemble -->
 				<div class="col-md-6">
@@ -160,11 +141,9 @@
 						</div>
 					</div>
 				</div>
-			</div>
 					<br>
 
 <!-- Jubilation Choir -->
-			<div class="row">
 					<div class="col-md-6">
 					<div class="card card-default">
 					<br>
@@ -187,7 +166,7 @@
 				</div>
 
 <!-- SoundCloud -->
-				<div class="col-md-6">
+				<div class="col-md-12">
 					<iframe width="100%" height="350" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/136812277&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
 				</div>
 				<br>
