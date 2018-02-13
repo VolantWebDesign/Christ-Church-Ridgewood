46c12f72a623b7c1001c5b869b32654c2e07024d
Tyler Stahl <tyler@volantweb.com>
2018-02-13 10:45:07 -0500

    Activated SoundCloud feed at Fr. Tom's request

--- index.php
+++ index.php
@@ -306,9 +306,9 @@
 				</div>
 
 <!-- SoundCloud -->
-				<!-- <div class="col-md-12">
+				<div class="col-md-6">
 					<iframe width="100%" height="350" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/136812277&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
-				</div> -->
+				</div>
 				<br>
 			</div>
 
