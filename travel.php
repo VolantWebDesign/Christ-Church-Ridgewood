3084914d9ab56cff225c13d77cb85dcf042d923f
Tyler Stahl <tylerstahl@gmail.com>
2019-05-27 16:48:16 -0400

    add 2019 travel page

---
+++ travel.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Christ Church, Ridgewood New Jersey 07450</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/modern-business.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+
+    <!-- Icons -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <meta name="theme-color" content="#3b74fb">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+<?php $thekeys = rand(1,1000); ?>
+<?php include "navigation.php" ?>
+
+    <!-- Page Content -->
+    <div class="container">
+            <div class="col-lg-12">
+                <h1 class="page-header">
+                    <!-- Worship with us... -->
+                </h1>
+            </div>
+        <div class="row" align="center">
+            <div class="col"><img src="images/travel-images/assisi_rome_2019_banner.png" class="img-fluid"></div>
+  
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-md-6">
+                <h1>Assisi & Rome</h1>
+                <h3>October 13-21, 2019</h3>
+                Our 10th annual trip takes us back to fabulous Italy, where it all began for us a decade ago.
+                <h3>$3,499 per person (double occupancy) *</h3>
+                <h4>Singles available for a supplement of $629<br>
+                Price includes a tax-deductible donation of $250</h4>
+            </div>
+            <div class="col-md-6">
+                <div class="panel-primary">
+                    <div class="panel-heading">
+                        <div class="panel-title">Trip includes:</div>
+                    </div>
+                    <div class="panel-body">
+                        <ul>
+                            <li>Round trip, non-stop flights from New York</li>
+                            <li>4 nights in the 4-star Hotel La Posta Dei Donini in Umbria</li>
+                            <li>3 nights in the 4-star Quirinale Hotel Rome</li>
+                            <li>Ground transfers and baggage handling in Italy</li>
+                            <li>Breakfast every day, plus first and last night dinners, three lunches</li>
+                            <li>Guided tours and all entrance fees:
+                            Assisi, Spoleto, Arezzo, La Verna, Rome,
+                            Colosseum & Forum</li>
+                            <li>Visits to a winery and Perugia chocolate
+                            factory</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="panel-primary">
+                    <div class="panel-heading">
+                        <div class="panel-title">Forms & Downloads</div>
+                    </div>
+                    <div class="panel-body">
+                        <ul class="list-group" align="center">
+                          <a href="documents/travel-documents/Assisi & Rome 2019 RESERVATION  FORM.pdf" target="_blank">
+                            <li class="list-group-item">Application</li>
+                            </a>
+                          <a href="documents/travel-documents/Umbria & Rome 2019 itinerary.pdf" target="_blank">
+                            <li class="list-group-item">Travel Itinerary</li>
+                            </a>
+                          <a href="documents/travel-documents/Umbria & Rome 2019 Flyer.pdf" target="_blank">
+                            <li class="list-group-item">Umbria & Rome Flyer</li>
+                            </a>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="panel-primary">
+                    <div class="panel-heading">
+                        <div class="panel-title">Important:</div>
+                    </div>
+                    <div class="panel-body">
+                    <ul>
+                        <li>Strictly limited to 40 passengers</li>
+                        <li>Deposit of $500 per person due immediately, final payment by July 1, 2019</li>
+                        <li>Information meeting Thursday, May 30th at 7 PM at Christ Church Ridgewood</li>
+                    </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="panel-primary">
+                    <div class="panel-heading">
+                        <div class="panel-title">For More Information</div>
+                    </div>
+                    <div class="panel-body">
+                    <a href="mailto:christchurchtravel@gmail.com" target=_blank>christchurchtravel@gmail.com</a><br>
+                    Christ Episcopal Church<br>
+                    Ridgewood, NJ 07450<br>
+                    201.652.2350<br>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    <?php include "donate.php"; ?>
+
+    </div>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
