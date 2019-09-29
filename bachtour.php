98e04a1466276bad4798906f4e89c18b78a7e1c4
Tyler Stahl <tylerstahl@gmail.com>
2019-09-29 15:55:14 -0400

    bach tour 2020 page

---
+++ bachtour.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Christ Church, Ridgewood New Jersey 07450</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/modern-business.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+
+    <!-- Icons -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <meta name="theme-color" content="#3b74fb">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+<?php $thekeys = rand(1,1000); ?>
+<?php include "navigation.php" ?>
+
+    <!-- Page Content -->
+    <div class="container">
+        <hr>
+       
+        <div class="row">
+            <div class="col-md-6">
+                <h1>In the Footsteps of J.S. Bach</h1>
+                <h2>A Spring 2020 Tour to Germany</h2>
+                <h3>May 20-30 2020</h3>
+                Join Christ Church Organist Bill Trafka on a trip back in time 300 years to travel in the footsteps of Bach
+                <h3>$3,549 per person (double occupancy)*</h3>
+                <h4>Singles available for a supplement of $529</h4>
+                <div class="col" align="center">
+                     <a href="documents/Bach_tour_2020/Final Bach Tour 2020 .pdf" target="_blank" class="btn btn-primary">
+                        Registration Form
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="panel-primary">
+                    <div class="row" align="center">
+                        <div class="col"><img src="images/Bach_Tour_2020/bach_banner.png" class="img-fluid"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <hr>
+            <!-- Travel photo carousel -->
+		<div class="row">
+			<div class="col-md-12">
+				<div class="card">
+					<div class="card-header">
+						<h4>Join us in Germany!</h4>
+					</div>
+					<div class="card-body">
+						<div class="row">
+                        <div class="col-lg-5">
+								<div class="card-body">
+                                    <p>Including these private organ recitals:</p>
+                                    <ul>
+                                        <li> St. Blasius Church in Muelhausen</li>
+                                        <li>Bach Church in Arnstadt</li>
+                                        <li>St Thomas Church in Leipzig</li>
+                                        <li>St. Wenzel Church in Naumburg</li>
+                                        <li>St. George’s Church in Rötha on the historic Silbermann organ</li>
+                                        <li>Kreuzkirche Church in Störmthal on the historic Hildebrandt organ</li>
+                                        <li>Town Church of Wittenberg</li>
+                                        <li>Köthen Castle on the chapel organ</li>
+                                    </ul>
+								</div>
+							</div>
+							<div class="col-lg-7">
+								<div id="carouselWelcome" class="carousel slide" data-ride="carousel">
+									<div class="carousel-inner">
+										<div class="carousel-item active">
+											<img class="d-block w-100" src="images/Bach_Tour_2020/18582151_10158892720525314_8689037806966735721_n.jpeg" alt="">
+										</div>
+                                        <div class="carousel-item">
+											<img class="d-block w-100" src="images/Bach_Tour_2020/1507163_10155782536385314_4677755979367950031_n_3.jpeg" alt="organist">
+										</div>
+										<div class="carousel-item">
+											<img class="d-block w-100" src="images/Bach_Tour_2020/18664618_10158906935790314_7429275221083961649_n.jpeg" alt="">
+										</div>
+									</div>
+								</div>
+
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+        <hr>
+        <!-- forms and downloads -->
+        <div class="row">
+            <div class="col-md-4">
+                <div class="panel-primary">
+                    <div class="panel-heading">
+                        <div class="panel-title">Forms & Downloads</div>
+                    </div>
+                    <div class="panel-body">
+                        <ul class="list-group" align="center">
+                          <a href="documents/Bach_tour_2020/Final Bach Tour 2020 .pdf" target="_blank">
+                            <li class="list-group-item">Itinerary and Registration Form</li>
+                            </a>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="panel-primary">
+                    <div class="panel-heading">
+                        <div class="panel-title">Important:</div>
+                    </div>
+                    <div class="panel-body">
+                    <ul>
+                        <li>Tour will be cancelled with fewer than 15 travelers</li>
+                        <li>Deposit of $750 per person, final payment by April 10, 2020</li>
+                        <li>Not included: flight to Europe, tips, drinks</li>
+                    </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="panel-primary">
+                    <div class="panel-heading">
+                        <div class="panel-title">For More Information</div>
+                    </div>
+                    <div class="panel-body">
+                    Contact tour host: Goodspeed & Bach<br>
+                    <a href="https://goodspeedandbach.com/bach-tour-2020" target="_blank">www.goodspeedandbach.com/bach-tour-2020</a><br>
+                    <a href="mailto:travel@goodspeedandbach.com" target=_blank>travel@goodspeedandbach.com</a><br>
+                    <a href="tel:8026522045"> (802)-652-2045</a><br>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
+
+    <?php include "donate.php"; ?>
+
+    </div>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
